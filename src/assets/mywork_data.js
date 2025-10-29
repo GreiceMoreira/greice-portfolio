fa67f2fa11dd64921d85280ab1865fc5d8f6c477
Greice Moreira <greice.f.d.a@hotmail.com>
2025-10-29 15:59:57 -0400

    Images correction

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,9 @@
-import project1_img from '../assets/project1.jpg'
-import project2_img from '../assets/project2.jpg'
-import project3_img from '../assets/project3.jpg'
-import project4_img from '../assets/project4.jpg'
-import project5_img from '../assets/project5.jpg'
-import project6_img from '../assets/project6.jpg'
+import project1_img from '../assets/project1.png'
+import project2_img from '../assets/project2.png'
+import project3_img from '../assets/project3.png'
+import project42_img from '../assets/project42.jpg'
+import project32_img from '../assets/project32.jpg'
+import project55_img from '../assets/project55.jpg'
 
 
 const mywork_data = [
@@ -11,36 +11,36 @@ const mywork_data = [
         w_no:1,
         w_name:"Web Design",
         w_img: project1_img,
-        w_link: "https://github.com/GreiceMoreira/StyleGuide"
+        w_link: "https://github.com/GreiceMoreira/Web_Full-Stack"
     },
-    {
+        {
         w_no:2,
         w_name:"Web Design",
         w_img: project2_img,
-        w_link: "https://github.com/ChandlerMurphy/wdd430-team5-project"
+        w_link: "https://github.com/GreiceMoreira/mycalendar"
     },
         {
         w_no:3,
         w_name:"Web Design",
         w_img: project3_img,
-        w_link: "https://github.com/GreiceMoreira/Web_Full-Stack"
-    },
-        {
-        w_no:4,
-        w_name:"Web Design",
-        w_img: project4_img,
-        w_link: "https://github.com/GreiceMoreira/mycalendar"
-    },
-        {
-        w_no:5,
-        w_name:"Web Design",
-        w_img: project5_img,
         w_link: "https://github.com/GreiceMoreira/calendar-api"
     },
-        {
+    {
+        w_no:4,
+        w_name:"Web Design",
+        w_img: project32_img,
+        w_link: "https://github.com/GreiceMoreira/StyleGuide"
+    },
+    {
+        w_no:5,
+        w_name:"Web Design",
+        w_img: project42_img,
+        w_link: "https://github.com/ChandlerMurphy/wdd430-team5-project"
+    },
+    {
         w_no:6,
         w_name:"Web Design",
-        w_img: project6_img,
+        w_img: project55_img,
         w_link: "https://github.com/GreiceMoreira/Handcrafted_Haven"
     }
 ]
